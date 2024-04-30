995ee36efe67740efca4c3cf7796ae657fc37fe9
kian_nrt7 <kiannosratian@gmail.com>
2024-04-30 15:13:23 +0330

    modified some files

--- algorithm/src/App.js
+++ algorithm/src/App.js
@@ -1,28 +1,33 @@
-import './App.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import React from "react";
+import { Component } from "react";
 import ButtonsBox from './algorithm';
 import Arrays from './arrays';
 import { useState } from 'react';
+import { render } from "@testing-library/react";
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 
-function App() {
-  
-  return (
-    <div className="app col-xl-12 col-lg-12  col-md-12 col-sm-12 col-12">
-     
-        <div className='box col-xl-12 col-lg-12  col-md-12 col-sm-12 col-12'>
-                  <div className=' box-item box-algorithm-buttons col-lg-6 col-md-12 col-sm-12 col-12'>
-                      <h1>Choose Youre Algorithm</h1>
 
-                      <ButtonsBox />
-                  </div>
-                  <div className=' box-item box-algorithm-inputs col-lg-6 col-md-12 col-sm-12 col-12'>
-                     <h1>See How Algorithm Works </h1>
-                      <Arrays   />
-                  </div>
-        </div>
-    </div>
-  );
+
+class App extends Component {
+  render(){
+    return (
+      <div className="app col-xl-12 col-lg-12  col-md-12 col-sm-12 col-12">
+          <div className='box col-xl-12 col-lg-12  col-md-12 col-sm-12 col-12'>
+                    <div className=' box-item box-algorithm-buttons col-lg-6 col-md-12 col-sm-12 col-12'>
+                        <h1>Choose Youre Algorithm</h1>
+                        <ButtonsBox />
+                    </div>
+                    <div className=' box-item box-algorithm-inputs col-lg-6 col-md-12 col-sm-12 col-12'>
+                       <h1>See How Algorithm Works </h1>
+                        <Arrays   />
+                    </div>
+          </div>
+      </div>
+    );
+  }
+ 
 }
 
  export default App;
