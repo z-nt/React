3a5b535ffc37e4869d14936fbe6aa662ccccffa8
kian_nrt7 <kiannosratian@gmail.com>
2024-05-07 10:10:20 +0330

    modified and delete some files

--- toturial/src/index.js
+++ toturial/src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import UserList from './testPara';
-
+import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <UserList/>
+    <App/>
 );
